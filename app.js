4025e61ce15240880b686ad0eaacc3c87d9fb7b3
meg-twilio <mcummings@twilio.com>
2020-02-14 13:16:33 -0800

    formatting app.js

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ const bodyParser = require('body-parser') //Used to parse requests so you can us
 const app = express()
 const routes = require("./src/api.js")
 const run = require("./src/service.js")
+const PORT = process.env.PORT || 8080;
 
-const PORT = 8080;
+// --------------------- MIDDLEWARE --------------------- //
 
+// Body Parser
 app.use(bodyParser.urlencoded({ extended: false}));// Thus lets express know to use static files
+
+// --------------------- ROUTES --------------------- //
 app.use('/', express.static('./'))
 
+
+// API Routes
 app.get('/getPhoneNumbers', routes.get_phone_numbers)
 app.get('/getBuyPhoneNumbers', routes.buy_phone_numbers)
 app.get('/getCreateMgService', routes.create_mg_service)
 app.get('/getAddPhoneNumbersToMgService', routes.add_phone_number_to_mg_service)
-
 app.get('/testAvailableNumbers', routes.get_phone_numbers)
 app.get('/testBuyPhoneNumbers', run.aquire_phone_numbers)
 app.get('/testCreateMgService', run.make_service)
 
-
+// --------------------- START SERVER --------------------- //
 app.listen(PORT, function() {
-	console.log("MG App Started on Port 8080")
+	console.log(`MG App Started on Port ${PORT}`)
 })

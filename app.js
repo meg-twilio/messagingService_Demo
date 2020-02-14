bf1f68dc66fdffdf61ed641108ee10408a02a8ad
meg-twilio <mcummings@twilio.com>
2020-02-14 13:30:42 -0800

    updated formatting

--- app.js
+++ app.js
@@ -8,23 +8,21 @@ const PORT = process.env.PORT || 8080;
 
 // --------------------- MIDDLEWARE --------------------- //
 
-// Body Parser
 app.use(bodyParser.urlencoded({ extended: false}));// Thus lets express know to use static files
 
 // --------------------- ROUTES --------------------- //
 app.use('/', express.static('./'))
 
-
 // API Routes
-app.get('/getPhoneNumbers', routes.get_phone_numbers)
-app.get('/getBuyPhoneNumbers', routes.buy_phone_numbers)
-app.get('/getCreateMgService', routes.create_mg_service)
-app.get('/getAddPhoneNumbersToMgService', routes.add_phone_number_to_mg_service)
-app.get('/testAvailableNumbers', routes.get_phone_numbers)
-app.get('/testBuyPhoneNumbers', run.aquire_phone_numbers)
-app.get('/testCreateMgService', run.make_service)
+app.get('/getPhoneNumbers', routes.get_phone_numbers);
+app.get('/getBuyPhoneNumbers', routes.buy_phone_numbers);
+app.get('/getCreateMgService', routes.create_mg_service);
+app.get('/getAddPhoneNumbersToMgService', routes.add_phone_number_to_mg_service);
+app.get('/testAvailableNumbers', routes.get_phone_numbers);
+app.get('/testBuyPhoneNumbers', run.aquire_phone_numbers);
+app.get('/testCreateMgService', run.make_service);
 
 // --------------------- START SERVER --------------------- //
 app.listen(PORT, function() {
 	console.log(`MG App Started on Port ${PORT}`)
-})
+});

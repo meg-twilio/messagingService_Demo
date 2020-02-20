00e592d7faee1d10b6910b1c928e528f6f5635a8
meg-twilio <mcummings@twilio.com>
2020-02-20 14:14:52 -0800

    updated phonenumbers show

--- controllers/phoneNumbersController.js
+++ controllers/phoneNumbersController.js
@@ -1,21 +1,10 @@
 const twilio = require('twilio')
 const client = new twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN) 
 
-function getTime() {
-    return new Date().toLocaleDateString();
-};
-
 module.exports = {
     show: (request, response) => {
-        client.availablePhoneNumbers(request.params.countryCode, (error, foundNumber) => {
-            if (error) return response.status(500).json({
-                status: 500,
-                error,
-                message: 'Something went wrong. please try again'
-            });
-
-            return response.json({ status: 200, message: foundNumber})
-        }).local
+        client.availablePhoneNumbers(request.params.countryCode)
+        .local
         .list({ areaCode: request.params.areaCode, limit: request.params.limit })
         .then(local => local.forEach(l => console.log(l.friendlyName)));
     }

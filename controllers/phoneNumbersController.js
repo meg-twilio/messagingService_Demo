2195f7304931a29082d8dba53c3cd481ba221a46
meg-twilio <mcummings@twilio.com>
2020-02-25 11:30:26 -0800

    added outbound call

--- controllers/phoneNumbersController.js
+++ controllers/phoneNumbersController.js
@@ -34,11 +34,11 @@ module.exports = {
     call: (request, response) => {
         console.log(request.params)
         client.calls.create({
-            twiml: <Response><Say voice="alice">Ahoy! Twilio is calling you!</Say></Response>,
-            to: '+19045371699',
-            from: '+9046839073'
+            to: request.params.phoneNumber,
+            from: '+19046829073',
+            url: "https://handler.twilio.com/twiml/EHc2bd22efbe2940182019d3ef7dc5be26" // TwiML Bin URL. 
         })
         .then(call => console.log(call.sid))
-        return response.json({ status: 200, message: `A phone call was made Successfully.` })
+        return response.json({ status: 200, message: 'Successfully called ' })
     }
 }
